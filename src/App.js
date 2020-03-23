0203f483c21f27bb42fe4efdfc6fa5755858ca41
Austin Parvin <austin.parvin@gmail.com>
2020-03-23 09:32:05 -0400

    button works, explorer done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
 
-class App extends Component {
+export class App extends Component {
+  state = {
+    roll: '?',
+  }
+
+  rollDice = (e) => {
+    const roll = Math.floor(Math.random() * 7)
+    console.log(roll)
+    this.setState({
+      roll: roll,
+    })
+  }
+
   render() {
-    return <HelloWorld />
+    return (
+      <main>
+        <p className="result">{this.state.roll}</p>
+        <button onClick={this.rollDice} className="roll-button">
+          Roll D6
+        </button>
+      </main>
+    )
   }
 }
 
